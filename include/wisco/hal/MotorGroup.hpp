95c6b8a3daf72daa212165c41e54e43ab128f03a
Ender717 <nathan.sandvig@gmail.com>
2024-02-21 10:37:05 -0600

    Added motor groups

---
+++ include/wisco/hal/MotorGroup.hpp
@@ -0,0 +1,99 @@
+#ifndef WISCO_HAL_MOTOR_GROUP_HPP
+#define WISCO_HAL_MOTOR_GROUP_HPP
+
+#include <memory>
+#include <vector>
+
+#include "wisco/io/IMotor.hpp"
+
+/**
+ * @brief Namespace for all library code
+ * @author Nathan Sandvig
+ *
+ */
+namespace wisco
+{
+
+/**
+ * @brief The namespace for the hardware abstraction layer
+ * @author Nathan Sandvig
+ *
+ */
+namespace hal
+{
+
+/**
+ * @brief A group of motors on the same connected output
+ *        SHOULD ONLY BE USED WITH IDENTICAL MOTORS
+ * @author Nathan Sandvig
+ * 
+ */
+class MotorGroup
+{
+private:
+    /**
+     * @brief The motors in the group
+     * 
+     */
+    std::vector<std::unique_ptr<io::IMotor>> motors{};
+
+public:
+    /**
+     * @brief Adds a motor to the motor group
+     * 
+     * @param motor The motor being added to the group
+     */
+    void addMotor(std::unique_ptr<io::IMotor>& motor);
+
+    /**
+     * @brief Initializes the motors
+     * 
+     */
+    void initialize();
+
+    /**
+     * @brief Get the torque constant of the motors
+     * 
+     * @return double The torque constant of the motors
+     */
+    double getTorqueConstant();
+
+    /**
+     * @brief Get the resistance of the motors
+     * 
+     * @return double The resistance of the motors
+     */
+    double getResistance();
+
+    /**
+     * @brief Get the angular velocity constant of the motors
+     * 
+     * @return double The angular velocity constant of the motors
+     */
+    double getAngularVelocityConstant();
+
+    /**
+     * @brief Get the gear ratio of the motors (1 if n/a)
+     * 
+     * @return double The gear ratio of the motors
+     */
+    double getGearRatio();
+
+    /**
+     * @brief Get the angular velocity of the motors in radians/second
+     * 
+     * @return double The angular velocity of the motors in radians/second
+     */
+    double getAngularVelocity();
+
+    /**
+     * @brief Set the voltage input to the motors in Volts
+     * 
+     * @param volts The voltage input in Volts
+     */
+    void setVoltage(double volts);
+};
+} // namespace hal
+} // namespace wisco
+
+#endif
---
+++ src/wisco/hal/MotorGroup.cpp
@@ -0,0 +1,72 @@
+#include "wisco/hal/MotorGroup.hpp"
+
+namespace wisco
+{
+namespace hal
+{
+void MotorGroup::addMotor(std::unique_ptr<io::IMotor>& motor)
+{
+    motors.push_back(std::move(motor));
+}
+
+void MotorGroup::initialize()
+{
+    for (auto& motor : motors)
+        motor->initialize();
+}
+
+double MotorGroup::getTorqueConstant()
+{
+    double average_constant{};
+    for (auto& motor : motors)
+        average_constant += motor->getTorqueConstant();
+    average_constant /= motors.size();
+
+    return average_constant * motors.size();
+}
+
+double MotorGroup::getResistance()
+{
+    double average_resistance{};
+    for (auto& motor : motors)
+        average_resistance += motor->getResistance();
+    average_resistance /= motors.size();
+    
+    return average_resistance;
+}
+
+double MotorGroup::getAngularVelocityConstant()
+{
+    double average_constant{};
+    for (auto& motor : motors)
+        average_constant += motor->getAngularVelocityConstant();
+    average_constant /= motors.size();
+    
+    return average_constant;
+}
+
+double MotorGroup::getGearRatio()
+{
+    double gear_ratio{};
+    if (!motors.empty())
+        gear_ratio = motors.front()->getGearRatio();
+    return gear_ratio;
+}
+
+double MotorGroup::getAngularVelocity()
+{
+    double average_velocity{};
+    for (auto& motor : motors)
+        average_velocity += motor->getAngularVelocity();
+    average_velocity /= motors.size();
+
+    return average_velocity;
+}
+
+void MotorGroup::setVoltage(double volts)
+{
+    for (auto& motor : motors)
+        motor->setVoltage(volts);
+}
+} // namespace hal
+} // namespace wisco
