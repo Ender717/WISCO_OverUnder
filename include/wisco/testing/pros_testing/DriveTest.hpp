bfe7c8f99a33a1959c23afc450884d70cf089dfe
Ender717 <nathan.sandvig@gmail.com>
2024-03-04 22:40:59 -0600

    Created a testing class for the drive

---
+++ include/wisco/testing/pros_testing/DriveTest.hpp
@@ -0,0 +1,162 @@
+#ifndef WISCO_TESTING_PROS_DRIVE_TEST_HPP
+#define WISCO_TESTING_PROS_DRIVE_TEST_HPP
+
+#include <cmath>
+#include <fstream>
+#include <memory>
+
+#include "pros/imu.hpp"
+#include "pros/motor_group.hpp"
+#include "pros/rotation.hpp"
+#include "pros/rtos.hpp"
+
+/**
+ * @brief Namespace for all library code
+ * @author Nathan Sandvig
+ *
+ */
+namespace wisco
+{
+
+/**
+ * @brief Namespace for all testing functions
+ * @author Nathan Sandvig
+ * 
+ */
+namespace testing
+{
+
+/**
+ * @brief Namespace for pros-based testing functions
+ * @author Nathan Sandvig
+ * 
+ */
+namespace pros_testing
+{
+
+/**
+ * @brief The path for writing all pros testing files
+ * 
+ */
+static constexpr char FILE_PATH[]{"/usd/testing/"};
+
+/**
+ * @brief Tests a pros-based drive
+ * @author Nathan Sandvig
+ * 
+ */
+class DriveTest
+{
+private:
+    /**
+     * @brief The name of the output file for linear drive testing
+     * 
+     */
+    static constexpr char LINEAR_FILE_NAME[]{"drive_linear_test.csv"};
+
+    /**
+     * @brief The name of the output file for turning drive testing
+     * 
+     */
+    static constexpr char TURNING_FILE_NAME[]{"drive_turning_test.csv"};
+
+    /**
+     * @brief Converts milliseconds to seconds
+     * 
+     */
+    static constexpr double MILLIS_TO_S{1.0 / 1000};
+
+    /**
+     * @brief Converts heading to radians
+     * 
+     */
+    static constexpr double HEADING_TO_RADIANS{-M_PI / 18000};
+
+    /**
+     * @brief Converts inches to meters
+     * 
+     */
+    static constexpr double INCHES_TO_METERS{2.54 / 100};
+
+    /**
+     * @brief Converts Volts to milliVolts
+     * 
+     */
+    static constexpr uint32_t V_TO_MV{1000};
+
+    /**
+     * @brief The number of millivolts to use for testing
+     * 
+     */
+    static constexpr uint8_t TEST_V{8};
+
+    /**
+     * @brief The duration of the tests in ms
+     * 
+     */
+    static constexpr uint32_t TEST_DURATION{1000};
+
+    /**
+     * @brief The left drive motors
+     * 
+     */
+    std::unique_ptr<pros::MotorGroup> m_left_drive_motors{};
+
+    /**
+     * @brief The right drive motors
+     * 
+     */
+    std::unique_ptr<pros::MotorGroup> m_right_drive_motors{};
+
+    /**
+     * @brief The heading sensor
+     * 
+     */
+    std::unique_ptr<pros::Imu> m_heading_sensor{};
+
+    /**
+     * @brief The linear distance tracking sensor
+     * 
+     */
+    std::unique_ptr<pros::Rotation> m_linear_sensor{};
+
+    /**
+     * @brief The CPI of the linear distance tracking sensor
+     * 
+     */
+    double m_linear_counts_per_inch;
+
+public:
+    /**
+     * @brief Construct a new Drive Test object
+     * 
+     * @param left_drive_motors The left drive motors
+     * @param right_drive_motors The right drive motors
+     * @param heading_sensor The heading sensor
+     * @param linear_sensor The linear distance tracking sensor
+     * @param linear_counts_per_inch The lienar distance tracking sensor CPI
+     */
+    DriveTest(std::unique_ptr<pros::MotorGroup>& left_drive_motors, 
+              std::unique_ptr<pros::MotorGroup>& right_drive_motors, 
+              std::unique_ptr<pros::Imu>& heading_sensor, 
+              std::unique_ptr<pros::Rotation>& linear_sensor, 
+              double linear_counts_per_inch);
+
+    /**
+     * @brief Runs the linear motion test
+     * 
+     */
+    void runLinearTest();
+
+    /**
+     * @brief Runs the turning motion test
+     * 
+     */
+    void runTurningTest();
+};
+
+} // namespace pros
+} // namespace testing
+} // namespace wisco
+
+#endif
---
+++ src/wisco/testing/pros_testing/DriveTest.cpp
@@ -0,0 +1,114 @@
+#include "wisco/testing/pros_testing/DriveTest.hpp"
+
+namespace wisco
+{
+namespace testing
+{
+namespace pros_testing
+{
+DriveTest::DriveTest(std::unique_ptr<pros::MotorGroup>& left_drive_motors, 
+                     std::unique_ptr<pros::MotorGroup>& right_drive_motors, 
+                     std::unique_ptr<pros::Imu>& heading_sensor, 
+                     std::unique_ptr<pros::Rotation>& linear_sensor, 
+                     double linear_counts_per_inch) :
+    m_left_drive_motors{std::move(left_drive_motors)},
+    m_right_drive_motors{std::move(right_drive_motors)},
+    m_heading_sensor{std::move(heading_sensor)},
+    m_linear_sensor{std::move(linear_sensor)},
+    m_linear_counts_per_inch{linear_counts_per_inch}
+{
+
+}
+
+void DriveTest::runLinearTest()
+{
+    std::string test_output_file_path{FILE_PATH};
+    test_output_file_path = test_output_file_path.append(LINEAR_FILE_NAME);
+    std::ofstream test_output_file{test_output_file_path};
+    if (test_output_file.fail())
+        return;
+
+    test_output_file << "linear_velocity,time\n";
+    test_output_file.flush();
+
+    if (m_left_drive_motors && m_right_drive_motors && m_heading_sensor && m_linear_sensor)
+    {
+        m_left_drive_motors->move_voltage(TEST_V * V_TO_MV);
+        m_right_drive_motors->move_voltage(TEST_V * V_TO_MV);
+
+        uint32_t start_time{pros::millis()};
+        double last_position{m_linear_sensor->get_position() / m_linear_counts_per_inch * INCHES_TO_METERS};
+        double last_time{pros::millis() * MILLIS_TO_S};
+        while (pros::millis() - start_time < TEST_DURATION)
+        {
+            double current_position{m_linear_sensor->get_position() / m_linear_counts_per_inch * INCHES_TO_METERS};
+            if (current_position != last_position)
+            {
+                double position_change{current_position - last_position};
+                last_position = current_position;
+
+                double current_time{pros::millis() * MILLIS_TO_S};
+                double time_change{current_time - last_time};
+                last_time = current_time;
+
+                double velocity{position_change / time_change};
+                test_output_file << velocity << ',' << current_time << '\n';
+                test_output_file.flush();
+            }
+            pros::delay(1);
+        }
+
+        m_left_drive_motors->move_voltage(0);
+        m_right_drive_motors->move_voltage(0);
+    }
+    
+    test_output_file.close();
+}
+
+void DriveTest::runTurningTest()
+{
+    std::string test_output_file_path{FILE_PATH};
+    test_output_file_path = test_output_file_path.append(TURNING_FILE_NAME);
+    std::ofstream test_output_file{test_output_file_path};
+    if (test_output_file.fail())
+        return;
+
+    test_output_file << "angular_velocity,time\n";
+    test_output_file.flush();
+
+    if (m_left_drive_motors && m_right_drive_motors && m_heading_sensor && m_linear_sensor)
+    {
+        m_left_drive_motors->move_voltage(-TEST_V * V_TO_MV);
+        m_right_drive_motors->move_voltage(TEST_V * V_TO_MV);
+
+        uint32_t start_time{pros::millis()};
+        double last_rotation{m_heading_sensor->get_rotation() * HEADING_TO_RADIANS};
+        double last_time{pros::millis() * MILLIS_TO_S};
+        while (pros::millis() - start_time < TEST_DURATION)
+        {
+            double current_rotation{m_heading_sensor->get_rotation() * HEADING_TO_RADIANS};
+            if (current_rotation != last_rotation)
+            {
+                double rotation_change{current_rotation - last_rotation};
+                last_rotation = current_rotation;
+
+                double current_time{pros::millis() * MILLIS_TO_S};
+                double time_change{current_time - last_time};
+                last_time = current_time;
+
+                double velocity{rotation_change / time_change};
+                test_output_file << velocity << ',' << current_time << '\n';
+                test_output_file.flush();
+            }
+            pros::delay(1);
+        }
+
+        m_left_drive_motors->move_voltage(0);
+        m_right_drive_motors->move_voltage(0);
+    }
+    
+    test_output_file.close();
+}
+} // namespace pros_testing
+} // namespace testing
+} // namespace wisco
