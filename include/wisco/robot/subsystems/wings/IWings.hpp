6ed1518a5bce542d398fab695c783276bec31a30
Ender717 <nathan.sandvig@gmail.com>
2024-04-12 11:34:59 -0500

    Created blank wings interface

---
+++ include/wisco/robot/subsystems/wings/IWings.hpp
@@ -0,0 +1,51 @@
+#ifndef WISCO_ROBOT_SUBSYSTEMS_WINGS_I_WINGS_HPP
+#define WISCO_ROBOT_SUBSYSTEMS_WINGS_I_WINGS_HPP
+
+/**
+ * @brief Namespace for all library code
+ * @author Nathan Sandvig
+ *
+ */
+namespace wisco
+{
+
+/**
+ * @brief The namespace that holds all robot classes
+ * @author Nathan Sandvig
+ *
+ */
+namespace robot
+{
+
+/**
+ * @brief Namespace for all robot subsystems
+ * @author Nathan Sandvig
+ * 
+ */
+namespace subsystems
+{
+
+/**
+ * @brief Namespace for all wings subsystem classes
+ * @author Nathan Sandvig
+ * 
+ */
+namespace wings
+{
+
+/**
+ * @brief Interface for wings subsystems
+ * @author Nathan Sandvig
+ * 
+ */
+class IWings
+{
+public:
+
+};
+} // namespace wings
+} // namespace subsystems
+} // namespace robot
+} // namespace wisco
+
+#endif
